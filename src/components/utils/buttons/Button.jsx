e271182df002596717b12ca7e6f422ff5e2fac40
MpourmoghaddamZand <mortezapourmoghaddam99@gmail.com>
2025-06-11 19:14:49 +0330

    ProductInfo done

--- src/components/utils/buttons/Button.jsx
+++ src/components/utils/buttons/Button.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Button = ({ children, link, className = "" }) => {
+const Button = ({ children, link, className = "", onClick }) => {
   return (
     <>
       {link !== undefined ? (
         <Link to={link}>
           {/* Using a div to style the button, as Link does not support styling directly */}
-          <div className={`text-center py-4 rounded-full w-10/12 m-auto text-white bg-mysecondary ${className}`}>
+          <div
+            className={`text-center py-4 rounded-full w-10/12 m-auto text-white bg-mysecondary ${className}`}
+          >
             {children}
           </div>
         </Link>
       ) : (
-          <div className={`text-center py-4 rounded-full w-10/12 m-auto text-white bg-mysecondary cursor-pointer ${className}`}>
+        <div
+          onClick={onClick}
+          className={`text-center py-4 rounded-full w-10/12 m-auto text-white bg-mysecondary cursor-pointer ${className}`}
+        >
           {children}
         </div>
       )}
--- src/components/utils/inputs/InputNumber.jsx
+++ src/components/utils/inputs/InputNumber.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaCircle, FaShoppingCart } from "react-icons/fa";
+import { FaCircleMinus, FaCirclePlus } from "react-icons/fa6";
 
 const InputNumber = () => {
-  return (
-    <div>InputNumber</div>
-  )
-}
+  const [value, setValue] = useState(0);
 
-export default InputNumber
+  const handleChange = (change) => {
+    if (change === "plus") {
+      setValue((prev) => prev + 1);
+    } else if (value > 0 && change === "minus") {
+      setValue((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <div className="p-3 drop-shadow-lg px-5 rounded-full flex  items-center justify-between gap-5 bg-white">
+      <FaCirclePlus
+        size={36}
+        color="#B67C47"
+        onClick={() => handleChange("plus")}
+      />
+      <div>
+        <p className="text-4xl pt-1 font-black">{value}</p>
+      </div>
+      <FaCircleMinus size={36} onClick={() => handleChange("minus")} />
+    </div>
+  );
+};
+
+export default InputNumber;
